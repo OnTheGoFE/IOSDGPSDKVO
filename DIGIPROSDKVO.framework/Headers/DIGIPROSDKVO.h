40fce2acf31725ddfcb3e10e6ef083c29773c6bf
Jonathan Viloria M <jonathanv@digipro.com.mx>
2020-05-30 10:53:34 -0500

    Init

---
+++ DIGIPROSDKVO.framework/Headers/DIGIPROSDKVO.h
@@ -0,0 +1,19 @@
+//
+//  DIGIPROSDKVO.h
+//  DIGIPROSDKVO
+//
+//  Created by Jonathan Viloria M on 5/7/19.
+//  Copyright © 2019 Jonathan Viloria M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DIGIPROSDKVO.
+FOUNDATION_EXPORT double DIGIPROSDKVOVersionNumber;
+
+//! Project version string for DIGIPROSDKVO.
+FOUNDATION_EXPORT const unsigned char DIGIPROSDKVOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DIGIPROSDKVO/PublicHeader.h>
+
+
